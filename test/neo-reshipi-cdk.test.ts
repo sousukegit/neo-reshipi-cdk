7c351ac1b5593e7dc3a961bca00f58c312560336
sousukegit <greenneosoul.sousuke@gmail.com>
2023-12-26 07:04:43 +0900

    Initial commit

---
+++ test/neo-reshipi-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NeoReshipiCdk from '../lib/neo-reshipi-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/neo-reshipi-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NeoReshipiCdk.NeoReshipiCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
